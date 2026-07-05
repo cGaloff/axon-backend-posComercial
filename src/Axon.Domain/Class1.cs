3c62afb4ac9c3ffb7be86180dffcfb7f4f0604e7
cGaloff <galofrexox@gmail.com>
2026-07-05 16:17:02 -0500

    chore: initial project structure with Clean architecture

---
+++ src/Axon.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Axon.Domain;
+
+public class Class1
+{
+
+}
---
+++ tests/Axon.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Axon.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
